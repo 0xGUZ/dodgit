cac8832708af5328255637f0dab1677408776936
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-01-27 18:22:34 -0300

    implemented player drag move for x asis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float xMin = -2.4f;
-    public float xMax = 2.4f;
+    public float xMin = -2f;
+    public float xMax = 2f;
     
     public GameObject gameController;
 
-    Rigidbody2D rb;
+    //touch move stuff
+    private Vector3 touchPos;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed = 10f;
 
     void Start()
     {
@@ -16,18 +20,23 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        //mobile movement => if is touching "follow" if not stop / drag move (ONLY X ASIS)
         if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0;
+            touchPos.y = transform.position.y; // keep the y position constant
+            direction = (touchPos - transform.position);
+            rb.velocity = new Vector2 (direction.x, 0) * moveSpeed; // only use the x component of the velocity
 
-            if(touch.phase == TouchPhase.Moved)
+            if(touch.phase == TouchPhase.Ended)
             {
-                float xPos = touch.position.x;
-                xPos = Mathf.Clamp(xPos, xMin, xMax);
-                rb.MovePosition(new Vector2(xPos, transform.position.y));
+                rb.velocity = Vector2.zero;
             }
         }
 
+        //move with keyboard -- mainly to test
         else {
             float xPos = transform.position.x;
             if (Input.GetKey(KeyCode.A))
@@ -43,6 +52,7 @@ public class Player : MonoBehaviour
         }
     }
 
+    //call die function if meteor hits player
     void OnTriggerEnter2D (Collider2D col) {
         if (col.CompareTag("Meteor") == true) {
             gameController.GetComponent<LevelControl>().Die();
