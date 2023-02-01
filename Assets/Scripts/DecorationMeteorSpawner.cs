081e5c022ad960b972b6a159379be0ed335afe70
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-01-31 23:21:26 -0300

    meteor update: fix the way meteors fall: now its by adding speed not letting gravity act so it means smoother in all fps, i tried to add some new meteors i will now have to add a meteor selector at the beginning of each level

--- Assets/Scripts/DecorationMeteorSpawner.cs
+++ Assets/Scripts/DecorationMeteorSpawner.cs
@@ -3,23 +3,14 @@ using UnityEngine;
 public class DecorationMeteorSpawner : MonoBehaviour
 {
     public GameObject meteorPrefab; // the meteor prefab to be spawned
-    private float spawnInterval = 1.4f; // interval between spawning meteors
+    private float spawnInterval = 0.5f; // interval between spawning meteors
     public float spawnDistance = 2.3f; // distance at which meteors spawn
 
     private float spawnTimer = 0.0f; // timer for spawning meteors
-    private float difficultyTimer = 0.0f; // timer for increasing difficulty
-    private float difficultyInterval = 10.0f; // interval for increasing difficulty
 
     void Update()
     {
         spawnTimer += Time.deltaTime; // increment spawn timer
-        difficultyTimer += Time.deltaTime; // increment difficulty timer
-
-        if (difficultyTimer >= difficultyInterval)
-        {
-            difficultyTimer = 0.0f; // reset difficulty timer
-            spawnInterval -= 0.1f; // decrease spawn interval
-        }
 
         if (spawnTimer >= spawnInterval)
         {
--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour
 {
+    public float speed = 8f;
+     
     void Update()
     {
+    	transform.position += Vector3.down * speed * Time.deltaTime;
+    	
         if (transform.position.y <= -30f){
             Destroy(gameObject);
         }
