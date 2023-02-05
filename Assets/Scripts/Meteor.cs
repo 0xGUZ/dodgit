d686d6f2e6c1a622b75246a98cb41789efeb97ca
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-02-05 17:59:15 -0300

    performance fix: delete meteor after 10f

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 
-using UnityEngine;
-
 public class Meteor : MonoBehaviour
 {
     public float speed = 8f;
 
     void Update()
     {
-        transform.position += Vector3.down * speed * Time.deltaTime;
+        transform.position += Vector3.down * speed * Time.deltaTime; 
+
+        if (transform.position.y <= -10f){
+            Destroy(gameObject);
+        }
     }
 }
