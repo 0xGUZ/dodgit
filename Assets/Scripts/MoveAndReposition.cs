f91f85543ae9d24f3f60ea4081acb8ae71415cca
Gustavo Villar <105249607+gustavo-villar-dev@users.noreply.github.com>
2023-02-05 01:29:19 -0300

    addition: stars
    new game addition: stars, make game more beautiful but no extra mechanics

---
+++ Assets/Scripts/MoveAndReposition.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MoveAndReposition : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float speed = 0.001f;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        foreach (Transform child in transform)
+        {
+            float x = Random.Range(mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x, mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x);
+            float y = Random.Range(mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y, mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y);
+
+            child.position = new Vector3(x, y, 0f);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (Transform child in transform)
+        {
+            child.position += new Vector3(0f, -speed, 0f);
+
+            if (child.position.y < mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y)
+            {
+                float x = Random.Range(mainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x, mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x);
+                float y = mainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y;
+
+                child.position = new Vector3(x, y, 0f);
+            }
+        }
+    }
+}
